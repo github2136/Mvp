a87c19923e9bcfe552a43ad3b799a59e3aefc76c
github2136 <github2136@163.com>
2018-12-07 11:44:57 +0800

    增加分页功能

--- app/src/main/java/com/github2136/mvp/ui/view/IListView.kt
+++ app/src/main/java/com/github2136/mvp/ui/view/IListView.kt
@@ -1,12 +1,10 @@
 package com.github2136.mvp.ui.view
 
-import androidx.paging.PagedListAdapter
-import com.github2136.base.IBaseMVPListView
 import com.github2136.base.IBaseMVPView
 
 /**
  * Created by yb on 2018/11/28.
  */
-interface IListView : IBaseMVPListView  {
+interface IListView : IBaseMVPView  {
 
 }
