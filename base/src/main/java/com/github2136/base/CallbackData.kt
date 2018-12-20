717c09aa10f6884e78ccace9d01ccdf08846d5c8
github2136 <github2136@163.com>
2018-12-20 10:51:38 +0800

    增加分页功能

--- base/src/main/java/com/github2136/base/CallbackData.kt
+++ base/src/main/java/com/github2136/base/CallbackData.kt
@@ -3,5 +3,4 @@ package com.github2136.base
 /**
  *  Created by yb on 2018/11/10.
  **/
-data class CallbackData( val isSuccess: Boolean, val str: String?) {
-}
+data class CallbackData(val isSuccess: Boolean, val str: String?)
